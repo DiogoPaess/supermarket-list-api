19326f9c2dd00a58cda06eddc46b551a2e81a80e
Diogo Paes <diogo.1@hotmail.com>
2023-12-30 13:10:13 +0000

    feat: configure .env files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ListItem = require("../models/ListItem");
+const ListItem = require("../models/listItem");
 const router = express.Router();
 
 router.get("/", (req, res) => {
